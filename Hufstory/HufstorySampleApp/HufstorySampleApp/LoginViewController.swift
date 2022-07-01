f0162c5633c6d13395d81328b1ccbe8dc5d6de4c
DongHyeok Lim <zx07050@naver.com>
2022-07-01 21:25:47 +0900

    initial commit
    기본 View 구성 중

---
+++ Hufstory/HufstorySampleApp/HufstorySampleApp/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  HufstorySampleApp
+//
+//  Created by 임동혁 on 2022/07/01.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+}
---
+++ Hufstory/HufstorySampleApp/HufstorySampleApp/startViewController.swift
@@ -0,0 +1,26 @@
+//
+//  startViewController.swift
+//  HufstorySampleApp
+//
+//  Created by 임동혁 on 2022/07/01.
+//
+
+import UIKit
+
+class startViewController: UIViewController {
+    @IBOutlet weak var emailLoginButton: UIButton!
+    @IBOutlet weak var googleLoginButton: UIButton!
+    @IBOutlet weak var appleLoginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        [emailLoginButton, googleLoginButton, appleLoginButton].forEach {
+            $0?.layer.borderWidth = 1
+            $0?.layer.borderColor = UIColor.black.cgColor
+            $0?.layer.cornerRadius = 30
+        }
+    }
+    
+
+}
