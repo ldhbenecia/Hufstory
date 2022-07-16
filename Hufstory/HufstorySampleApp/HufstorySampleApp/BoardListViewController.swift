867973dcc505827e9a0230d476d93b5f6c78552d
DongHyeok Lim <zx07050@naver.com>
2022-07-16 19:00:27 +0900

    [feat] 게시판 목록 ViewController 생성

---
+++ Hufstory/HufstorySampleApp/HufstorySampleApp/BoardListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BoardList.swift
+//  HufstorySampleApp
+//
+//  Created by 임동혁 on 2022/07/16.
+//
+
+import UIKit
+
+class BoardListController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
